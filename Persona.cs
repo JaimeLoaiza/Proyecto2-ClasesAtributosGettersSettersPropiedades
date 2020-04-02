ebfe0af077f73a0676d3d5fbcc8d1fd18bc9a208
JaimeLoaiza <jaimeloaiza@hotmail.es>
2020-04-01 20:35:08 -0500

    Proyecto2

---
+++ Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreaciónClasesAtributosGettersSettersPropiedades
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        //private string numeroDeDocumento;
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string Apellido
+        {
+            get
+            {
+                return this.apellido;
+            }
+            set
+            {
+                this.apellido = value;
+            }
+        }
+        public string numeroDeDocumento { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreaciónClasesAtributosGettersSettersPropiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona objeto = new Persona();
+            Console.WriteLine("Escriba el Nombre");
+            objeto.SetNombre(Console.ReadLine());
+            Console.WriteLine("Escriba el Apellido");
+            objeto.Apellido = Console.ReadLine();
+            Console.WriteLine("Escriba el nombre del Documento");
+            objeto.numeroDeDocumento = Console.ReadLine();
+            Console.WriteLine("Los Datos Ingresados Fueron:");
+            Console.WriteLine("Nombre {0}", objeto.GetNombre());
+            Console.WriteLine("Apellido {0}", objeto.Apellido);
+            Console.WriteLine("Número de Documento {0}", objeto.numeroDeDocumento);
+            Console.ReadKey();
+        }
+    }
+}
